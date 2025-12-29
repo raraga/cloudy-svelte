7a391ab18e976e6ed48dc26b33cbaf1e3bbc7c55
Renato Araga <raraga@gmail.com>
2025-12-28 19:56:24 -0800

    updated deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>SvelteKit built with workers!</h1>
+<h1>SvelteKit built with Cloudflare workers!</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
